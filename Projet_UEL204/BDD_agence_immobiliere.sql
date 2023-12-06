6341fda157305177a3e239248b04dcc5173d9f47
franssouah <118920717+franssouah@users.noreply.github.com>
2023-12-06 10:42:16 +0100

    Update BDD_agence_immobiliere.sql

--- Projet_UEL204/BDD_agence_immobiliere.sql
+++ Projet_UEL204/BDD_agence_immobiliere.sql
@@ -48,17 +48,25 @@ INSERT INTO `clients` (`id`, `identifiant`, `motdepasse`) VALUES
 CREATE TABLE `logements` (
   `id` int(11) NOT NULL,
   `adresse` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `ville`varcar(255) COLLATE utf8_unicode_ci NOT NULL,
   `type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `surface` int(11) NOT NULL
+  `surface` int(11) NOT NULL,
+  `prix` int (11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `logements`
 --
 
-INSERT INTO `logements` (`id`, `adresse`, `type`, `surface`) VALUES
-(1, '12 avenue Albert Thomas, 87000 Limoges', 'appartement', 67),
-(2, '3 avenue de Landouge, 87100 Limoges', 'maison', 137);
+INSERT INTO `logements` (`id`, `adresse`, `ville`, `type`, `surface`,`prix`) VALUES
+(1, '12 avenue Albert Thomas, 87000 Limoges', 'Limoges', 'appartement', 67, 175000),
+(2, '3 avenue de Landouge, 87100 Limoges', 'Limoges', 'maison', 137, 230000),
+(3, "7 rue d'Alsace, 87220 Feytiat", 'Feytiat', 'appartement', 48, 90000),
+(4, '47 avenue du Limousin, 87220 Feytiat', 'Feytiat', 'maison', 98, 182000),
+(5, '31 rue de la Garde, 87270 Couzeix', 'Couzeix', 'maison', 75, 190000),
+(6, '8, rue de la Garenne, 87430 Verneuil-sur-Vienne', 'Verneuil-sur-Vienne', 'maison', 125, 260000),
+(7, '14 rue de Bellevue, 87270 Couzeix', 'Couzeix', 'appartement', 45, 85000),
+(8, '16 allée des troubadours, 87430 Verneuil-sur-Vienne', 'Verneuil-sur-Vienne', 'maison', 180, 320000);
 
 --
 -- Indexes for dumped tables
