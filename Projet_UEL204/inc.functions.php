64045a3bbf3525c88f32921cef98c3b0ac00f7a9
AmandaHPrieto <105066159+AmandaHPrieto@users.noreply.github.com>
2023-12-10 17:51:56 +0200

    Extranalisation de la fonction afficher tous les biens.

--- Projet_UEL204/inc.functions.php
+++ Projet_UEL204/inc.functions.php
@@ -1,4 +1,5 @@
 <?php
+	include('pages/inc.connexion.php');
 	
 	/*
 		Fonction hasArticleInSession
@@ -121,5 +122,85 @@
 		}
 		return false;
 	}
+
+
+
+
+	// Amanda
+	// Fonction pour afficher tous les logements de la bdd
+
+	function afficherTousLogements(){
+		// On rend la variable bdd globale
+		global $bdd;
+	
+		/* On peut lire la requête ci-dessous comme cela :
+		   La requête ('query') est de sélectionner (SELECT) tout (=> *) ce qui est dans la table 'logements' 
+		   de la BDD définie par $dbb et de stocker toute la réponse dans la variable $requete.
+		   Ce qui est contenu dans la variable $requete n'est pas exploitable. Il faut aller plus loin. */
+		$requete = $bdd->query('SELECT * FROM logements');
+		
+		/* On va traiter la réponse ($requete) entrée par entrée avec une boucle while.
+		   On va aller chercher (=> fetch) chaque entrée de la table successivement et on va stocker les valeurs
+		   dans un tableau associatif $data qui contient les valeurs de chaque champ pour toutes les entrées.
+		   On accède alors aux identifiants de cette façon => $data['identifiant']. */
+		while ($data = $requete->fetch())
+		{
+			if (!$data) // On teste si la réponse à la requête est vide.
+			{
+				echo 'La BDD n\'existe pas ou est vide.';
+				break;
+			}
+			else
+			{
+				// echo 'Adresse : '.$data['titre'].'<br>';
+				echo 'Identifiant : '.$data['id'].' - Adresse : '.$data['adresse'].' - Type : '.$data['type'].' - Surface : '.$data['surface'].'m² - Prix : '.$data['prix'].'€<br>';
+			}
+		}
+		/* La requête fetch renvoie un booléen faux ('false') lorsqu'on est arrivé à la fin des données.
+		   La boucle while s'arrête donc. 
+		   La ligne ci-dessous indique qu'il faut "fermer le curseur qui parcourt les données".
+		   Il est impératif de le faire afin d'éviter tout problème lors de la requête suivante. */
+		$requete->closeCursor();
+	}
+
+	// Fonction pour afficher les résultats d'un recherche par ville
+	function recherche(){
+	// On rend la variable bdd globale
+	global $bdd;
+	
+	// Pour la récupération d'une partie de la BDD, on utilise la méthode des requêtes préparées permettant de se prémunir contre les injections SQL
+	// On utilise la méthode "prepare" de l'extension PDO afin de préparer une requête à être lancée (http://php.net/manual/fr/pdo.prepare.php)
+	// Puis la méthode "execute" de l'extension PDO pour lancer la requête après avoir inséré les paramètres (http://php.net/manual/fr/pdo.exec.php)
+	
+	// Etape 1 : on prépare la requête (plusieurs exemples ci-dessous, testez chacune d'elles !)
+	$requete = $bdd -> prepare('SELECT id, adresse, surface, ville, prix,  FROM logements WHERE ville = ?');
+	// $requete = $bdd->prepare('SELECT identifiant, auteur, titre, annee, exemplaires FROM bibliotheque WHERE auteur = ? ORDER BY annee'); // Il est ici implicite que c'est un classement par ordre croissant.
+	// $requete = $bdd -> prepare('SELECT identifiant, auteur, titre, annee, exemplaires FROM bibliotheque WHERE auteur = ? ORDER BY annee DESC'); // Pour un classement par ordre décroissant
+	// $requete = $bdd -> prepare('SELECT identifiant, auteur, titre, annee, exemplaires FROM bibliotheque WHERE auteur = ? ORDER BY annee ASC LIMIT 0,2');
+	// $requete = $bdd -> prepare('SELECT identifiant, auteur, titre, annee, exemplaires FROM bibliotheque WHERE auteur = ? AND annee > 1850');
+	
+	// Exécution de la requête via la méthode 'execute'
+	//$requete->execute(array($_POST['ville'])); // Le paramètre indiqué va aller remplacer le '?' de la ligne précédente
+	$requete->execute('Limoges'); // Le paramètre indiqué va aller remplacer le '?' de la ligne précédente
+	
+	/* 
+	A noter que si on a besoin d'utiliser 2 paramètres, on fait par exemple comme ceci :
+	$requete = $bdd -> prepare('SELECT identifiant, auteur, titre, annee, exemplaires FROM bibliotheque WHERE auteur = ? AND annee > ?');
+	$requete->execute(array($_POST['nom_auteur'],$_POST['annee_minimum']));
+	*/
+	
+	/* Une autre syntaxe évitant les '?' est possible (plus lisible lorsqu'il y a beaucoup de paramètres) :
+	$requete = $bdd -> prepare('SELECT id, adresse, ville, surface, prix FROM logements WHERE ville = :ville AND prix > :prix');
+	$requete->execute(array('ville' => $_POST['ville'], 'prix' => $_POST['prix_minimum'])); */
+	
+	while ($data = $requete->fetch())
+	{	
+		echo 'Identifiant : '.$data['id'].' - Adress : '.$data['adresse'].' - Type : '.$data['type'].' - Surface : '.$data['surface'].' - Prix : '.$data['prix'].'<br>';
+	}
+	$requete->closeCursor();
+}
+
+recherche()
+
 
 ?>
--- Projet_UEL204/pages/inc.connexion.php
+++ Projet_UEL204/pages/inc.connexion.php
@@ -1,9 +1,10 @@
 <?php
-	// Param�tres de connexion
+	// Paramètres de connexion
 	$serveur  = "localhost:3306";
 	$database = "BDD_agence_immobiliere";
 	$user     = "ue_l204";
 	$password = "airphp";
+	
 	
 	// CONNEXION A LA BASE DE DONNEES mysql //
 	/* 
@@ -15,25 +16,21 @@
 	try
 	{	
 		/* 
-			PDO est une extension "orient�e objet". Il faut donc v�rifier que l'extension PDO est bien activ�e sur votre version de PHP (cf cours)
-			On travaille en local : localhost
-			La BDD s'appelle "UE_L204"
-			Le login par d�faut est "root"
-			Le mot de passe (sur MAMP) est aussi "root". Il peut �tre vide pour WampServer.
-			On cr�e un objet $bdd
+			PDO est une extension "orientée objet". Il faut donc vérifier que l'extension PDO est bien activée sur votre version de PHP (cf cours)
 		*/
 
-		// Connexion � la base de donn�es
-		// $bdd est un objet correspondant � la connexion � la BDD
+		// Connexion à la base de données
+		// $bdd est un objet correspondant à la connexion à la BDD
 		$bdd=new PDO('mysql:host='.$serveur.';charset=utf8;dbname='.$database.'',$user,$password);
 		
-		/* La ligne ci-dessous permet d'activer les erreurs lors de la connexion � la BDD via PDO.
-		Les messages d'erreur li� � des requ�tes SQL comportant des erreurs seront plus clairs. */
+		
+		/* La ligne ci-dessous permet d'activer les erreurs lors de la connexion à la BDD via PDO.
+		Les messages d'erreur liés à des requêtes SQL comportant des erreurs seront plus clairs. */
 		array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 	}
 	catch(Exception $e)
 	{
-		// On lance une fonction PHP qui permet de conna�tre l'erreur renvoy�e lors de la connection � la base (en r�cup�rant le message li� � l'exception)
+		// On lance une fonction PHP qui permet de connaître l'erreur renvoyée lors de la connection à la base (en récupérant le message lié à l'exception)
 		die('<strong>Erreur détectée !!! </strong> : ' . $e->getMessage());
 	}
 ?>
