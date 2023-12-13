4d38edfa1ff87b9da8837dcf99ab920ec268407b
AnnyRzn <152983780+AnnyRzn@users.noreply.github.com>
2023-12-13 16:37:12 +0100

    mise en forme menu
    définitions des couleurs et des fonts de base, correction dans les fichiers index.php et menu.php, ajout photo bandeau

--- Projet_UEL204/index.php
+++ Projet_UEL204/index.php
@@ -11,17 +11,20 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="icon" type="image/png" href="png"/>
 		<link href="assets/styles.css" rel="stylesheet">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Poppins&family=Roboto&family=Sniglet&display=swap" rel="stylesheet">
 	</head>
 
 	<body>
 
-		<header class="bandeau">
+		<header class="bandeau ">
 			<?php 
 			include('menu.php');
 			?>
-			<h1>AirPHP: les bonnes bases pour votre maison!</h1>
 		</header>
 <section>
+	<h1>AirPHP: les bonnes bases pour votre maison!</h1>
 	<div class="form">
 		<form method="POST" action="pages/resultats.php">
 			<fieldset>
