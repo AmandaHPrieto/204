37a54bb807a9381c49ac30bdad02fb83b9147a6a
EstelleDab <152979607+EstelleDab@users.noreply.github.com>
2023-12-15 15:21:54 +0100

    securistation sur surface

--- Projet_UEL204/inc.functions.php
+++ Projet_UEL204/inc.functions.php
@@ -18,22 +18,38 @@
 	/* fonction permettant de créer une boite à favoris si elle n'existe pas déjà et d'ajouter un logement dedasn*/ 
 
 
-	function addFavoriToSession( $photo, $adresse, $ville, $categorie, $surface, $prix){
+	function addFavoriToSession($photo, $adresse, $ville, $categorie, $surface, $prix  ){
+
 		if(!favorisInSession()){
-			$_SESSION['favoris'] = array();
+			$_SESSION['favoris']=array();
 		}
-		$_favori = array(
-			'photo' => ('<img class="img-maison" src="../assets/photos/'.$photo.'">'),
-			'adresse' =>	($adresse),
-			'ville' =>	('<span class="span-ville">'.$ville.'</span>'),
-			'categorie' =>	('<span class="span-categorie">'.$categorie.'</span>'),
-			'surface'=> ('<span class="span-surface">'.$surface.'</span>'),
-			'prix'  =>	('<span class="span-prix">'.$prix.'</span>')
-		);
-	
-		array_push($_SESSION['favoris'], $_favori);
+
 		
-	}
+			$_favori = array(	
+				'photo' => ('<img class="img-maison" src="../assets/photos/'.$photo.'">'),
+				'adresse' =>	($adresse),
+				'ville' =>	('<span class="span-ville">'.$ville.'</span>'),
+				'categorie' =>	('<span class="span-categorie">'.$categorie.'</span>'),
+				'surface'=> ('<span class="span-surface">'.$surface.'</span>'),
+				'prix'  =>	('<span class="span-prix">'.$prix.'</span>')
+			);
+
+array_push($_SESSION['favoris'], $_favori);	
+	echo" logement a bien été ajouté à vos favoris";	
+		}
+		
+
+		
+	
+	
+			
+					
+				
+
+
+		
+	
+	
 	
 	/*
 		Fonction isConnecte
@@ -46,7 +62,9 @@
 				&& array_key_exists('login', $_SESSION)
 					&& !empty($_SESSION['login'])){
 			return true;
-		}else{
+		}
+		
+		else{
 			return false;
 		}
 	}
@@ -86,7 +104,6 @@
 		if(!array_key_exists('msg', $_SESSION)){
 			$_SESSION['msg'] = "";
 		}
-
 		$_SESSION['msg'] .= $msg." ";
 	}
 
@@ -104,25 +121,28 @@
 	
 
 // Fonction pour afficher les résultats d'une recherche
-function recherche(){
+	function recherche(){
 	// On rend la variable bdd globale
 	global $bdd;
-	//if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['ville'], $_POST['surface'], $_POST['budget'])) à voir si on l'utilise 
-	$requete ='SELECT * FROM logements WHERE ville="'.$_POST['ville'].'" AND surface > "'.htmlspecialchars($_POST['surface']).'" AND prix < "'.$_POST['budget'].'"'; //visiblement si le input surface n'est pas rempli c'est pas grave pour la recherche??? 
-	$param_categ = ''; //les checkbox porte le meme nom mais une valeur différente (appartement ou maison): leur nom est un déclaré comme un tableau. 
-//ainsi les valeurs cochées seront transmises sous forme de tableau que l'on pourra parcourir pour les récupérer. (super pratique car tu peux avoir autant de valeurs que tu veux! On peut mettre des péniches et des yourtes aussi!)
+		if ($_SERVER['REQUEST_METHOD'] === 'POST'
+			&&  !empty($_POST['ville'])
+					&& !empty($_POST['budget'])
+							&& is_numeric($_POST['surface'])) {
+	
+			$requete ='SELECT * FROM logements WHERE ville="'.$_POST['ville'].'" AND surface > "'.$_POST['surface'].'" AND prix < "'.$_POST['budget'].'"'; //visiblement si le input surface n'est pas rempli c'est pas grave pour la recherche??? 
+			$param_categ = ''; 	//les checkbox porte le meme nom mais une valeur différente (appartement ou maison): leur nom est un déclaré comme un tableau. 
+								//ainsi les valeurs cochées seront transmises sous forme de tableau que l'on pourra parcourir pour les récupérer. (super pratique car tu peux avoir autant de valeurs que tu veux! On peut mettre des péniches et des yourtes aussi!)
 		
-	foreach($_POST['categorie'] as $valeur ) { //on parcourt les valeurs de notre tableau de retours checkbox.
-		$param_categ .= 'categorie="'.$valeur.'" OR '; 
-	}
-	//echo $param_categ; je te laisse le echo comme ça tu vois l'apercu. 
-	if($param_categ != '' ) {
-		$requete .= ' AND ('.substr($param_categ, 0, -4).')'; // on retire le OR qui va s'ajouter à la fin de la boucle
-	}
+			foreach($_POST['categorie'] as $valeur ) { //on parcourt les valeurs de notre tableau de retours checkbox.
+			$param_categ .= 'categorie="'.$valeur.'" OR '; 
+			}
+			
+			if($param_categ != '' ) {
+			$requete .= ' AND ('.substr($param_categ, 0, -4).')'; // on retire le OR qui va s'ajouter à la fin de la boucle
+		}
 
-	//echo $requete;  en faisant cela on peut récupérer la requete et la tester dans l'ongler sql de php my admin directement.
-	$requete=$bdd->prepare($requete);
-	$requete->execute();
+			$requete=$bdd->prepare($requete);
+			$requete->execute();
 
 		while ($logement = $requete->fetch()){
 			if (!$logement) // On teste si la réponse à la requête est vide.
@@ -135,7 +155,6 @@ function recherche(){
 				echo '<div class="conteneur-maison">';
 				/*si l'utilisateur est connecté, un coeur apparaît et peut ajouter un logement à ses favoris*/
 				if (isConnecte()){
-					
 					echo '<a href="?logement='.$logement['id'].'"><img src="./assets/images/favoris.png" width="30px" alt="favoris "></a>'; /*attention ici lien pour récupérer les données de chaque logement à l'ajout aux favoris */
 				}
 				$photo='<img class="img-maison" src="assets/photos/'.$logement['photo'].'">';
@@ -161,16 +180,19 @@ function recherche(){
 			echo '</div></div>';
 		}
 	$requete->closeCursor();	
+}
 };
 
 
+function message($message){
+	echo $message;
+}
 //fonction ajout fav
 function addFavori(){ 
 	// On rend la variable bdd globale
 	global $bdd;
-
-	if($_GET && count($_GET)){
 	
+	if($_GET && count($_GET)){
 		if(array_key_exists('logement', $_GET) && !empty($_GET['logement'])){
 			$id=$_GET['logement'];
 			$request =$bdd->query('SELECT * FROM logements WHERE id='.$id.'');
@@ -182,25 +204,14 @@ function addFavori(){
 				$categorie=$id['categorie'];
 				$surface=($id['surface']);
 				$prix=$id['prix'];
-	
-				favorisInSession();
-				addFavoriToSession($photo, $adresse, $ville,$categorie, $surface, $prix);
+		
 			}
-		}
-	} 
-$requete->closeCursor();	
+			
+				favorisInSession();
+				addFavoriToSession($photo, $adresse, $ville, $categorie, $surface, $prix);			
+		
+} 
+	$requete->closeCursor();	
 }
-  /* $file_write='mesfavoris.txt';
-    $write=fopen($file_write, 'a');
-    foreach ($_SESSION['favoris'] as $_favori)
-        {
-        file_put_contents($file_write, $_favori PHP_EOL, FILE_APPEND);
-        }
-     fclose($write);
-                    
-    $_file_read='mesfavoris.txt';
-    $read=fopen($file_read,'r');
-     $text= file_get_contents('mesfavoris.txt');
-    echo $text;*/
-
+}
 ?>
